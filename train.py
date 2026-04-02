166aa9b73fa9565da165ce8bbc5d22ad5032865c
Mohit Uniyal <mohituniyal2010@gmail.com>
2026-04-02 08:12:49 +0530

    train decision tree classifier

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 
-model = LogisticRegression()
+model = DecisionTreeClassifier()
 model.fit(X_train, y_train)
